b0e5af0ac46427555bca6c172111eaf7287b07d9
AlexandrMakarenko <45914266+AlexandrMakarenko@users.noreply.github.com>
2020-01-30 16:52:35 +0300

    новая версия
    Изменена сцена главного меню

---
+++ Assets/Vvod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Vvod : MonoBehaviour
+{
+
+
+    public void SetSymbol()
+    {
+        for (int handIndex = 0; handIndex < Player.instance.hands.Length; handIndex++)
+        {
+            Hand hand = Player.instance.hands[handIndex];
+            if (hand != null)
+            {
+                
+            }
+        }
+    }
+}
